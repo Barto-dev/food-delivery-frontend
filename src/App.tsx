e218f53b651cdb9ccc056dae20e519b8fc65958d
Illia <illya1004work@gmail.com>
2022-09-15 23:59:31 +0100

    chore: added apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './styles/apollo';
 
 function App() {
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello Tailwind</h1>
-    </div>
-  )
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1 className="text-3xl font-bold underline">Hello Tailwind</h1>
+      </div>
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
